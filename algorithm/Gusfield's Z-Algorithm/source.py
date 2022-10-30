642a1cf04fb8712d219ce6cc575412adc37b3baa
Mill <yushuguo89757@gmail.com>
2022-10-31 02:25:15 +1100

    Encapsulate Gusfield's Z-algorithm
    Build everything inside a class and provide a way to test it separately

--- algorithm/Gusfield's Z-Algorithm/source.py
+++ algorithm/Gusfield's Z-Algorithm/source.py
@@ -25,67 +25,82 @@ def naive_algorithm(pattern: str, text: str) -> int:
             return i
 
 
-def z_algorithm(pattern: str, text: str) -> int:
-    """ Implement Gusfield's Z-algorithm to find the
-        first occurrence of input substring. "$" is used
-        as the combined operator in this case.
+class GusfieldZ:
+    def __init__(self, text: str, pattern: str = None):
+        self.pattern = pattern
+        self.text = text
+        self.concatenated_str = None
+        self.z_values = None
 
-    Arg:
-        :param text: the substring with length >= 2
-        :param pattern: the original string
-    Rtn:
-        :return: the index of first occurrence of pattern
-
-    Time complexity (Worst Case): P(m+n)
-    """
-    n = len(text)
-    m = len(pattern)
-    concatenated_str = pattern + "$" + text
-    length = m + n + 1
-
-    z_values = [0 for _ in range(length)]
-    l, r = 0, 0
-    k = 1
-    while k < length:
-        # Case 1
-        if k > r:
-            z_k = 0
-            for i in range(length-k):
-                if concatenated_str[i] != concatenated_str[k+i]:
-                    break
-                else:
-                    z_k += 1
-            if z_k > 0:
-                l, r = k, k + z_k - 1
-                z_values[k] = z_k
-        else:
-            # Case 2a
-            if z_values[k-l] < r-k+1:
-                z_values[k] = z_values[k-l+1]
-            # Case 2b
-            else:
-                assert z_values[k-l] >= r-k+1, "Z value is too small in Z algorithm Case 2b"
-                z_k = z_values[k-l]
-                for i in range(length-r-1):         # operation with length value and index value
-                    if concatenated_str[r+1+i] != concatenated_str[r-k+1+i]:
+    def generate_z_values(self) -> list:
+        assert self.concatenated_str is not None, "The pattern is empty"
+        length = len(self.concatenated_str)
+        z_values = [0 for _ in range(length)]
+        l, r = 0, 0
+        k = 1
+        while k < length:
+            # Case 1
+            if k > r:
+                z_k = 0
+                for i in range(length - k):
+                    if self.concatenated_str[i] != self.concatenated_str[k + i]:
                         break
                     else:
                         z_k += 1
-                z_values[k] = z_k
-                l, r = k, k + z_k - 1
-        k += 1
+                if z_k > 0:
+                    l, r = k, k + z_k - 1
+                    z_values[k] = z_k
+            else:
+                # Case 2a
+                if z_values[k - l] < r - k + 1:
+                    z_values[k] = z_values[k - l]
+                # Case 2b
+                else:
+                    assert z_values[k - l] >= r - k + 1, "Z value can not be increased since its value is too small"
+                    z_k = z_values[k - l]
+                    if z_values[k-l] + k > length:
+                        z_k = length - k
+                    else:
+                        for i in range(length - r - 1):  # operation with length value and index value
+                            if self.concatenated_str[r + 1 + i] != self.concatenated_str[r - k + 1 + i]:
+                                break
+                            else:
+                                z_k += 1
+                    z_values[k] = z_k
+                    l, r = k, k + z_k - 1
+            k += 1
+        return z_values
 
-    # Find out the first occurrence
-    for i, z_value in enumerate(z_values):
-        if z_value == m:
-            return i - 1 - m
+    def find(self, pattern: str):
+        """ Implement Gusfield's Z-algorithm to find the
+        first occurrence of input substring. "$" is used
+        as the combined operator in this case.
+
+        Arg:
+            :param pattern: the original string
+        Rtn:
+            :return: the index of first occurrence of pattern
+
+        Time complexity (Worst Case): P(m+n)
+        """
+        self.pattern = pattern
+        m = len(pattern)
+        self.concatenated_str = self.pattern + "$" + self.text
+        self.z_values = self.generate_z_values()
+
+        # Find out the first occurrence
+        for i, z_value in enumerate(self.z_values):
+            if z_value == m:
+                return i - 1 - m
 
 
 if __name__ == "__main__":
     word = "bbabaxababay"
     target = "aba"
     word = "sdafffsdffqewrfqewrdsf;lrewjgkregnkngjkjfdkhgjklrem,gfdjkhgbjkfdhbjkbnruigfiu2tqrqwr"
-    target = "wrfqewrdsf;lrewjgkregnkngjkjfd"
-    index = z_algorithm(target, word)
-    print(index)
-    assert word[index:index+len(target)] == target, "Failed"
+    target = "lrewjgkregnkngjkjfd"
+    p_gz = GusfieldZ(word, "acababacaba")
+    # index = p_gz.find(target)
+    p_gz.concatenated_str = "acababacaba"
+    print(p_gz.generate_z_values())
+    # assert word[index:index+len(target)] == target, "Failed"
